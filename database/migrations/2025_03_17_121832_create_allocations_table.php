c9291743a5493e99ba3c5a85ccda44c3c8a16c53
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-17 13:22:12 +0100

    created the weekdays table and allocations table migrations with the columns

---
+++ database/migrations/2025_03_17_121832_create_allocations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('allocations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('department_id')
+                        ->constrained('weekdays')
+                        ->onDelete('cascade');
+            $table->foreignId('employee_id')
+                        ->constrained('weekdays')
+                        ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('allocations');
+    }
+};
