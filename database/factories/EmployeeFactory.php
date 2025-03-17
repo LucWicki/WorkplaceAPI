ad4ad4ec50a0b41c0cb2c43e61309f2f9573efb6
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-17 13:28:07 +0100

    added is_chef to the definition in the EmployeeFactory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -29,6 +29,7 @@ class EmployeeFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_chef' => false
         ];
     }
 
