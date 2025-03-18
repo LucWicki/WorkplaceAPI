ce8ec5443a48dacde36ff4847f8d0311d9ae649e
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-18 14:06:22 +0100

    added the login routes and the users routes

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+
 
 //TODO: Routen definieren
+
+//login and logout routes
+Route::middleware(['web'])->group( function(){
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout']);
+});
+
+
+Route::middleware(['chef', 'auth:sanctum'])->group( function(){
+    Route::ApiResource('users', 'App\Http\Controllers\UserController');
+    Route::get('/users/displaydepartmentinfos',[UserController::class, 'displayDepartmentInfos']);
+});
