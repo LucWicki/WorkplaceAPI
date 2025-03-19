167b4c837f03a8aab3c42f4c3c0b226108c83c2a
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-19 16:50:32 +0100

    todo comment added to StoreUserRequest

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -20,10 +20,10 @@ class StoreUserRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-    { //TODO: add requirements to password rule
+    {//TODO: add requirements to password rule
         return [
             'username' => 'required|string|max:255',
-            'email' => 'required|unique:users,email|regex:/(.+)@(.+)\.(.+)/i',
+            'email' => 'required|unique:users,email|regex:/(.+)@(.+)\.(.+)/i', //TODO: add regex to also check for symbols besides . and -
             'password' => 'required',
             'is_chef' => 'required|boolean',
             'department_id' => 'required|integer|exists:departments,id'
