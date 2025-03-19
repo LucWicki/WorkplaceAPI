403df4deb339f7b335b8cec2461ec9c016513045
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-19 14:23:43 +0100

    added checkdataacces function to Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 abstract class Controller
 {
-    //
+    public function checkDataAccess($user_id){
+        return (Auth::id() != $user_id) ? true : false;
+    }
 }
