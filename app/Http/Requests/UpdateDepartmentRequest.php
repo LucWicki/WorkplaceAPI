92beb89151302c3fb571b3c869303b6f93047b9d
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-19 16:27:32 +0100

    fixed the departmentname rule as I forgot a ","

--- app/Http/Requests/UpdateDepartmentRequest.php
+++ app/Http/Requests/UpdateDepartmentRequest.php
@@ -20,9 +20,9 @@ class UpdateDepartmentRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
-    {
+    {//TODO: make a custom error message for unique
         return [
-            'departmentname' => 'nullable|string|max:255|unique:departments,departmentname'.$this->department->id,
+            'departmentname' => 'nullable|string|max:255|unique:departments,departmentname,'.$this->department->id,
             'departmentwebsite' => 'nullable|string', //TODO: maybe with regex or a website rule?
             'weekday_id' => 'nullable|integer|exists:weekdays,id',
         ];
