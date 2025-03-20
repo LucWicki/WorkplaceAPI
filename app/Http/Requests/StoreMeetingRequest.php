e697283c9cfa80f2263667e8c5efe9a3606efba2
Luc Wicki <luc.wicki@twofold.swiss>
2025-03-20 09:25:32 +0100

    update and storeRequest of meeting done

--- app/Http/Requests/StoreMeetingRequest.php
+++ app/Http/Requests/StoreMeetingRequest.php
@@ -11,7 +11,7 @@ class StoreMeetingRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreMeetingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'meetingname' => 'required|string|max:255|unique:meetings,meetingname',
+            'reason' => 'required|string|max:255',
+            'department_id' => 'required|integer|exists:departments,id',
         ];
     }
 }
--- app/Http/Requests/UpdateMeetingRequest.php
+++ app/Http/Requests/UpdateMeetingRequest.php
@@ -11,7 +11,7 @@ class UpdateMeetingRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateMeetingRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'meetingname' => 'nullable|string|max:255|unique:meetings,meetingname,'.$this->meeting->id,
+            'reason' => 'nullable|string|max:255',
+            'department_id' => 'nullable|integer|exists:departments,id',
         ];
     }
 }
